b2f0bd661b4e535fa06598341ca8a16dfdd2700c
iqbal <iqbalchoudhury2@gmail.com>
2016-08-07 23:15:02 -0500

    dependencies added

---
+++ Twitter/src/test/java/credential/Login.java
@@ -0,0 +1,21 @@
+package credential;
+
+import base.CommonAPI;
+import org.testng.annotations.Test;
+
+/**
+ * Created by a on 8/7/2016.
+ */
+public class Login extends CommonAPI {
+
+    @Test
+    public void submitLogin() throws InterruptedException{
+        clickByCss(".Button.StreamsLogin.js-login");
+        Thread.sleep(2000);
+        typeByCss(".text-input.email-input.js-signin-email","iqbalchoudhury2@gmail.com");
+      typeByXpath(".//*[@id='login-dialog-dialog']/div[2]/div[2]/div[2]/form/div[2]/input","Mangopeople1");
+        Thread.sleep(2000);
+        clickByXpath(".//*[@id='login-dialog-dialog']/div[2]/div[2]/div[2]/form/input[1]");
+        Thread.sleep(3000);
+    }
+}
